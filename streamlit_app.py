c710972d85c5fe135b33b977b2b64b7d266e996d
moopi <rainbowww5@gmail.com>
2023-08-07 17:25:24 +0800

    Update streamlit_app.py
    add text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
-streamlit.title('My pParents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
